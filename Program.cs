4fd1ba5ebb9ef603bd0cf00e8c0f0e392b148c98
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-23 23:15:26 +0530

    [Shantanu] Added : Main program file

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace Day15_BinarySearchTree
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello to Binary Search Tree!");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
         }
     }
 }
